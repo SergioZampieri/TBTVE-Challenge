9fc40b204d186f4c6768e39e6876b7ad5a1fd597
Sergio <gersio.zampieri@gmail.com>
2025-11-16 11:45:02 -0300

    core: webpack client migration (no vite)

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,4 +1,8 @@
-export default {
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
--- client/src/app.test.jsx
+++ client/src/app.test.jsx
@@ -1,4 +1,3 @@
 test('demo', () => {
   expect(true).toBe(true)
 })
-
